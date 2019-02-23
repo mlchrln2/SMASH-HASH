5c1f0f01a31e5b654213435df10a053a1b0ea398
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-02-23 00:55:52 -0600

    remove hyperparameters

--- Backend/ImageNet/hyperparameters.py
+++
@@ -1,8 +0,0 @@
-options = {}
-options['learning_rate'] = 1e-3
-options['batch_size'] = 1
-options['n_batches'] = 100
-options['n_layers'] = 5
-options['hidden_neurons'] = 128
-options['output_neurons'] = 2
-options['num_epochs'] = 100
