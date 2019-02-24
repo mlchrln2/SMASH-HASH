fcd9e72f819a796841ea5f0fc7bd57ec61523f13
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-02-23 18:25:03 -0600

    DataLoader now supports batching

--- Backend/ImageNet/ImageTrain.py
+++ Backend/ImageNet/ImageTrain.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.utils import save_image
@@ -10,10 +11,24 @@ import matplotlib.pyplot as plt
 from torchvision.transforms import ToPILImage
 from HyperParameters import options
 from NeuralNetworks import Autoencoder
+from NeuralNetworks import ImageEncoder
 from NeuralNetworks import Attention
 from tensorboardX import SummaryWriter
 import gc
 import sys
+
+def collate(data):
+	'''pads the data with 0's'''
+	images, captions = zip(*data)
+	length = torch.tensor([[item.size(1),item.size(2)] for item in images])
+	(max_i,max_j),_ = torch.max(length,0)
+	images = [nn.ZeroPad2d((0, max_j.item()-item.size(2), 0, max_i.item()-item.size(1)))
+			(item) for item in images]
+	images = torch.stack(images, 0)
+	return images, captions
+
+
+
 
 #create a logger
 writer = SummaryWriter('ImageNetSummary/')
@@ -29,16 +44,18 @@ to_img=ToPILImage()
 #dataset location in memory
 myPassport_dir = '/media/orlandomelchor/My Passport/datasets/coco-dataset'
 
-transform = transforms.Compose([
-	transforms.Resize(size=(640,640)),
-	transforms.ToTensor()
-])
+transform = transforms.transforms.ToTensor()
+
 #transform = transforms.ToTensor()
 #load data and captions in batches
 dataset = CocoCaptions(root='{}/train2017/'.format(myPassport_dir), 
 					   annFile='{}/annotations/captions_train2017.json'.format(myPassport_dir),
 					   transform=transform)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+dataloader = DataLoader(dataset=dataset, 
+						batch_size=batch_size,
+						collate_fn=collate,
+						shuffle=False,
+						drop_last=True)
 
 #initialize model and loss function
 model = None
@@ -47,7 +64,7 @@ if sys.argv[1] == 'continue':
     model = torch.load('img_embedding_model.sav')
 #to start training a new model type: "python filename restart"
 elif sys.argv[1] == 'restart':
-    model = Autoencoder()
+    model = ImageEncoder()
 
 for epoch in range(num_epochs):
 	error = 0
