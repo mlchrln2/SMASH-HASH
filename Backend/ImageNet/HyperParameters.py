54afb45bdddee270ce375103f6a8d2e4075393a1
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-02-25 16:59:08 -0600

    added extra parameters only for training

--- Backend/ImageNet/HyperParameters.py
+++ Backend/ImageNet/HyperParameters.py
@@ -1,9 +1,9 @@
-options = {}
-options['learning_rate'] = 1e-3
-options['batch_size'] = 32
-options['n_batches'] = 100
-options['n_layers'] = 5
-options['hidden_neurons'] = 128
-options['output_neurons'] = 2
-options['num_epochs'] = 100
-options['vocab_size'] = 1024
+options = {
+	'learning_rate'   : 1e-3,
+	'batch_size'      : 16, 
+	'n_batches'       : 100,
+	'num_epochs'      : 100,
+	'vocab_size'      : 53967,
+	'output_channels' : 96,
+	'max_len'		  : 100
+}
