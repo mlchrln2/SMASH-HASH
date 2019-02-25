b9607b934536dc351116a5af7d5a802b9006f79d
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-02-25 16:59:47 -0600

    improved the training scheme to reduce clutter and improve logging

--- Backend/ImageNet/ImageTrain.py
+++ Backend/ImageNet/ImageTrain.py
@@ -12,14 +12,13 @@ import matplotlib.pyplot as plt
 from torchvision.transforms import ToPILImage
 from HyperParameters import options
 from NeuralNetworks import Autoencoder
-from NeuralNetworks import ImageEncoder
+from NeuralNetworks import Image2Caption
 from NeuralNetworks import Attention
-from Vocabulary import word_dict
+from Vocabulary import word2idx
+from Vocabulary import idx2word
 from tensorboardX import SummaryWriter
 import gc
 import sys
-
-embedding = nn.Embedding(len(word_dict),100)
 
 def collate(data):
 	'''pads the data with 0's'''
@@ -29,12 +28,12 @@ def collate(data):
 	images = [nn.ZeroPad2d((0, max_j.item()-item.size(2), 0, max_i.item()-item.size(1)))
 			(item) for item in images]
 	images = torch.stack(images, 0)
-	length = torch.tensor([item.size(1) for item in captions])
-	max_cap = torch.max(length)
-	captions = [nn.ConstantPad1d((0, max_cap.item()-item.size(1)),0)
-			(item) for item in captions]
-	captions = torch.stack(captions, 0)
-	captions = embedding(captions)
+
+	length = torch.tensor([[item.size(0),item.size(1)] for item in captions])
+	(max_i,max_j),_ = torch.max(length,0)
+	captions = [nn.ZeroPad2d((0, max_j.item()-item.size(1), 0, max_i.item()-item.size(0)))
+			   (item) for item in captions]
+	captions = torch.stack(captions, 0)	
 	return images, captions
 
 #create a logger
@@ -54,7 +53,8 @@ myPassport_dir = '/media/orlandomelchor/My Passport/datasets/coco-dataset'
 class Word2Int(object):
 	def __call__(self,sentences):
 		max_sentence =  torch.max(torch.tensor([len(sentence.split(' ')) for sentence in sentences]))
-		words = torch.tensor([[word_dict[sentence.split(' ')[i]] if i < len(sentence.split(' ')) else 0 for i in range(max_sentence)] for sentence in sentences]).long()
+		words = torch.tensor([[word2idx[sentence.split(' ')[i]] if i < len(sentence.split(' ')) 
+							 else 0 for i in range(max_sentence)] for sentence in sentences]).long()
 		return words
 
 
@@ -80,19 +80,17 @@ if sys.argv[1] == 'continue':
     model = torch.load('img_embedding_model.sav')
 #to start training a new model type: "python filename restart"
 elif sys.argv[1] == 'restart':
-    model = ImageEncoder()
+    model = Image2Caption()
+print('Note model parameters:\n{}'.format(model.parameters))
 
 for epoch in range(num_epochs):
 	error = 0
 	gc.collect()
-	for i,(img,caption) in enumerate(dataloader):
-		rand_caption = torch.randint(high=caption.size(1),size=(1,))
-		caption = caption[:,rand_caption,:,:].squeeze(1)
-		print(img.size())
-		print(caption.size())
-		output = model(img,caption.size(1))
-		print(output.size())
-		loss = model.criterion(output, caption)
+	for i,(img,captions) in enumerate(dataloader):
+		rand_caption = torch.randint(high=captions.size(1),size=(1,))
+		captions = captions[:,rand_caption,:].squeeze(1)
+		output_captions, input_captions = model(img,captions)
+		loss = model.criterion(output_captions,input_captions)
 		loss.backward()
 		model.optimizer.step()
 		error += loss.detach().item()
