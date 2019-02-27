6ec6164d5f903f1eb2a88209cb173c6e1425240a
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-02-27 06:09:04 -0600

    renamed old parameters, added more for rnns and added tokens for word2idx and idx2word dictionary

--- Backend/ImageNet/HyperParameters.py
+++ Backend/ImageNet/HyperParameters.py
@@ -1,9 +1,14 @@
 options = {
-	'learning_rate'   : 1e-3,
-	'batch_size'      : 16, 
-	'n_batches'       : 100,
-	'num_epochs'      : 100,
-	'vocab_size'      : 53156,
-	'output_channels' : 96,
-	'max_len'		  : 100
+	'learning_rate' : 1e-3,
+	'batch_size'    : 8, 
+	'n_batches'     : 100,
+	'num_epochs'    : 100,
+	'vocab_size'    : 29549,
+	'embed_size'    : 256,
+	'max_len'	    : 100,
+	'bidirectional' : True,
+	'num_layers'    : 1,
+	'start_word'    : '<START>',
+	'end_word'      : '<END>',
+	'unk_word'      : '<UNK>'
 }
