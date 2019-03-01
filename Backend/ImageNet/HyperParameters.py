32c47f08eb3236f9ce835390f4525108739b4eac
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-01 12:11:05 -0600

    Added data directory parameter and added more hidden parameters to make the NN more versatile

--- Backend/ImageNet/HyperParameters.py
+++ Backend/ImageNet/HyperParameters.py
@@ -1,14 +1,17 @@
 options = {
 	'learning_rate' : 1e-3,
 	'batch_size'    : 8, 
-	'n_batches'     : 100,
 	'num_epochs'    : 100,
+	'channel_size'  : 256,
 	'vocab_size'    : 29549,
 	'embed_size'    : 256,
-	'max_len'	    : 100,
+	'hidden_size'   : 256,
 	'bidirectional' : True,
 	'num_layers'    : 1,
+	'max_len'	    : 25,
 	'start_word'    : '<START>',
 	'end_word'      : '<END>',
-	'unk_word'      : '<UNK>'
+	'unk_word'      : '<UNK>',
+	'data_dir'      : '../../CocoDataset'
+
 }
