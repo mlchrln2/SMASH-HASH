33e4562f067a73c9334f2fe42d920d4ac58004c6
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-01 12:12:39 -0600

    Now using the VGG16 encoder and made a few alterations to the inference method

--- Backend/ImageNet/NeuralNetworks.py
+++ Backend/ImageNet/NeuralNetworks.py
@@ -9,20 +9,22 @@ from HyperParameters import options
 class Image2Caption(nn.Module):
 	def __init__(self):
 		super(Image2Caption, self).__init__()
-		self.batches = options["batch_size"]
+		self.channel_size = options['channel_size']
 		self.embed_size = options['embed_size']
 		self.vocab_size = options['vocab_size']
 		self.max_len = options['max_len']
 		self.bidirectional = options['bidirectional']
 		self.num_layers = options['num_layers']
 		self.LR = options['learning_rate']
-		self.image_encoder = ImageEncoder(batches=self.batches, 
-									      embed_size=self.embed_size)
+		self.hidden_size = options['hidden_size']
+		self.image_encoder = ImageEncoder(channel_size=self.channel_size)
 		self.word_encoder = WordEncoder(vocab_size=self.vocab_size, 
 									    embed_size=self.embed_size,
+									    hidden_size=self.hidden_size,
 									    bidirectional=self.bidirectional, 
 									    num_layers=self.num_layers)
-		self.caption_decoder = CaptionDecoder(embed_size=self.embed_size,
+		self.caption_decoder = CaptionDecoder(hidden_size=self.hidden_size,
+											  channel_size=self.channel_size,
 									          vocab_size=self.vocab_size, 
 									          max_len=self.max_len,
 									          bidirectional=self.bidirectional,
@@ -32,51 +34,47 @@ class Image2Caption(nn.Module):
 										  weight_decay=1e-5)
 		self.criterion = nn.CrossEntropyLoss()
 		self.softmax = nn.Softmax(1)
-	def forward(self, image, captions):
-		features = self.image_encoder(image)
+	def forward(self, images, captions):
+		features = self.image_encoder(images)
 		captions = self.word_encoder(captions)
 		captions, summaries = self.caption_decoder(features,captions)
 		captions = self.softmax(captions.transpose(1,2))
 		return captions, summaries
 	def infer(self, image):
-		features = self.encoder(image)
-		caption = self.embedding(torch.zeros(1,dtype=torch.long))
-		captions = self.decoder.infer(features, caption)
-		return captions
+		features = self.image_encoder(image)
+		caption = self.word_encoder(torch.zeros((features.size(0),1),dtype=torch.long))
+		words = self.caption_decoder.infer(features, caption)
+		return words
 
 class ImageEncoder(nn.Module):
-	def __init__(self, batches, embed_size):
+	def __init__(self, channel_size):
 		super(ImageEncoder,self).__init__()
-		self.batches = batches
-		self.embed_size = embed_size
-		resnet = models.resnet50(pretrained=True)
-		modules = list(resnet.children())[:-1]
-		self.resnet = nn.Sequential(*modules)
-		self.embed = nn.Linear(in_features=resnet.fc.in_features, 
-							   out_features=self.embed_size)
-		self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+		self.channel_size = channel_size
+		pretrained_net = models.vgg16(pretrained=True).features
+		modules = list(pretrained_net.children())[:29]
+		self.pretrained_net = nn.Sequential(*modules)
+		self.out_channel = nn.Linear(in_features=modules[-1].in_channels,
+									 out_features=self.channel_size)
 	def forward(self, x):
 		with torch.no_grad():
-			x = self.resnet(x)
-		x = x.view(self.batches,x.size(1),-1)
+			x = self.pretrained_net(x)
+		x = x.view(x.size(0),x.size(1),-1)
 		x = x.transpose(1,2)
-		x = self.embed(x)
-		x = x.transpose(1,2)
-		x = self.bn(x)
-		x = x.transpose(1,2)
+		x = self.out_channel(x)
 		return x
 
 class WordEncoder(nn.Module):
-	def __init__(self, vocab_size, embed_size, bidirectional, num_layers):
+	def __init__(self, vocab_size, embed_size, hidden_size, bidirectional, num_layers):
 		super(WordEncoder, self).__init__()
 		self.vocab_size = vocab_size
 		self.embed_size = embed_size
+		self.hidden_size = hidden_size
 		self.bidirectional = bidirectional
 		self.num_layers = num_layers
 		self.embedding = nn.Embedding(num_embeddings=self.vocab_size, 
 									  embedding_dim=self.embed_size)
 		self.rnn = nn.GRU(input_size=self.embed_size,
-						  hidden_size=self.embed_size, 
+						  hidden_size=self.hidden_size, 
 						  bidirectional=self.bidirectional,
 						  num_layers=self.num_layers,
 						  batch_first=True)
@@ -86,40 +84,45 @@ class WordEncoder(nn.Module):
 		return embed
 
 class CaptionDecoder(nn.Module):
-	def __init__(self, embed_size, vocab_size, max_len, bidirectional, num_layers):
+	def __init__(self, hidden_size, channel_size, vocab_size, max_len, bidirectional, num_layers):
 		super(CaptionDecoder, self).__init__()
-		self.embed_size = embed_size
+		self.hidden_size = hidden_size
+		self.channel_size = channel_size
 		self.vocab_size = vocab_size
 		self.max_len = max_len
 		self.bidirectional = bidirectional
 		self.bi_factor = (2 if self.bidirectional else 1)
 		self.num_layers = num_layers
-		self.attention = Attention(query_size=self.embed_size,
-								   context_size=self.embed_size*self.bi_factor)
-		self.rnn = nn.GRU(input_size=self.embed_size,
-						  hidden_size=self.embed_size, 
+		self.attention = Attention(query_size=self.channel_size,
+								   context_size=self.hidden_size*self.bi_factor)
+		self.rnn = nn.GRU(input_size=self.channel_size,
+						  hidden_size=self.hidden_size, 
 						  bidirectional=self.bidirectional,
 						  num_layers=self.num_layers,
 						  batch_first=True)
-		self.word_decoder = WordDecoder(self.embed_size*self.bi_factor, 
+		self.word_decoder = WordDecoder(self.hidden_size*self.bi_factor, 
 										self.vocab_size)
 	def forward(self,x,hn):
 		x,x_weights = self.attention(x,hn)
 		hn = hn[:,0]
-		hn = hn.view(-1,self.bi_factor,self.embed_size)
+		hn = hn.view(-1,self.bi_factor,self.hidden_size)
 		x,hn = self.rnn(x,hn.transpose(0,1))
 		hn = hn.transpose(0,1).contiguous()
-		summaries = hn.view(-1,self.embed_size*self.bi_factor)
+		summaries = hn.view(-1,self.hidden_size*self.bi_factor)
 		x = self.word_decoder(x)
 		return x, summaries
 	def infer(self,features,caption):
-		words = torch.zeros(self.max_len,dtype=torch.long)
+		words = torch.ones(features.size(0),self.max_len,dtype=torch.long)
 		for i in range(self.max_len):
 			x,x_weights = self.attention(features,caption)
+			caption = caption.contiguous().view(-1,self.bi_factor,self.hidden_size)
 			caption = caption.transpose(0,1)
-			caption,hn = self.rnn(features,caption)
-			idx = torch.argmax(self.word_decoder(caption))
-			words[i] = idx
+			caption,hn = self.rnn(x,caption)
+			word_vec = self.word_decoder(caption).squeeze(1)
+			idxs = torch.argmax(word_vec,1)
+			words[:,i] = idxs
+			if idxs.item() == 1:
+				break
 		return words
 
 '''
@@ -128,11 +131,11 @@ General Soft-Attention Model
 class Attention(nn.Module):
 	'''
 		Attention parameters:
+			q_size: query size
 			c_size: context size
-			h_size: query size
 		Inputs:
+			q  : query of shape   (N,S,q_size)
 			c_t: context of shape (N,1,c_size)
-			q  : query of shape   (N,S,h_size)
 		Formulation:
 			score = c_t^T * W_a * q_i
 			alpha_ti = exp(score) / sum_{i=1}^{n}{exp(score)}
@@ -160,14 +163,15 @@ class Attention(nn.Module):
 		s_t = torch.bmm(alpha_t,q)
 		return s_t, W_attn
 	def score(self,q,c_t):
-		return torch.bmm(self.W_a(q),c_t.transpose(1,2))
+		a = torch.bmm(self.W_a(q),c_t.transpose(1,2))
+		return a
 
 class WordDecoder(nn.Module):
-	def __init__(self, embed_size, vocab_size):
+	def __init__(self, hidden_size, vocab_size):
 		super(WordDecoder, self).__init__()
-		self.embed_size = embed_size
+		self.hidden_size = hidden_size
 		self.vocab_size = vocab_size
-		self.decoder = nn.Linear(in_features=self.embed_size, 
+		self.decoder = nn.Linear(in_features=self.hidden_size, 
 								 out_features=self.vocab_size)
 	def forward(self, embed):
 		captions = self.decoder(embed)
