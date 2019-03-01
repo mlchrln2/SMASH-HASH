7e90c4a2485fa25a5d507a3a6e19cda8a52bde84
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-01 12:09:17 -0600

    Using a new encoder with different preprocessing. Also added a test loader for inference

--- Backend/ImageNet/DataLoader.py
+++ Backend/ImageNet/DataLoader.py
@@ -46,13 +46,15 @@ class CocoDataset(data.Dataset):
 		img_id = self.ids[index]
 		path = coco.loadImgs(img_id)[0]['file_name']
 		img = Image.open(os.path.join(self.root, path)).convert('RGB')
-		targets = self.coco_dataset[str(index)][:].astype(int)
+		target_loc = self.coco_dataset[str(index)]
+		rand_idx = torch.randint(target_loc.shape[0],size=(1,)).item()
+		targets = torch.from_numpy(target_loc[rand_idx].astype(int))
 		if self.transform is not None:
 			img = self.transform(img)
 		if self.target_transform is not None:
 			targets = self.target_transform(targets)
-		captions = targets[:,:-1]
-		lengths = targets[:,-1]
+		lengths = targets[-1]+2
+		captions = targets[:(lengths)]
 		return img, captions, lengths
 
 	def __len__(self):
@@ -61,50 +63,46 @@ class CocoDataset(data.Dataset):
 def collate(batch):
 	'''pads the data with 0's'''
 	images, captions,lengths = zip(*batch)
-	length = torch.tensor([[item.size(1),item.size(2)] for item in images])
-	(max_i,max_j),_ = torch.max(length,0)
-	images = [nn.ZeroPad2d((0, max_j.item()-item.size(2), 0, max_i.item()-item.size(1)))
-			(item) for item in images]
 	images = torch.stack(images, 0)
-
-	length = torch.tensor([[item.size(0),item.size(1)] for item in captions])
-	(max_i,max_j),_ = torch.max(length,0)
-	captions = [nn.ZeroPad2d((0, max_j.item()-item.size(1), 0, max_i.item()-item.size(0)))
+	lengths = torch.stack(lengths,0)
+	max_j = torch.max(lengths)
+	captions = [nn.ConstantPad1d((0, max_j.item()-item.size(0)),1)
 			   (item) for item in captions]
 	captions = torch.stack(captions, 0)
 	return images, captions, lengths
 
 #transform for data
 
-#transform = transforms.ToTensor()
 transform = transforms.Compose([ 
-    #transforms.Resize(256),                          # smaller edge of image resized to 256
-    #transforms.RandomCrop(224),                      # get 224x224 crop from random location
-    #transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
-    transforms.ToTensor()#,                           # convert the PIL Image to a tensor
-    #transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
-    #                     (0.229, 0.224, 0.225))
-    ])
-target_transform = torch.from_numpy
+	transforms.Resize(256),                          # smaller edge of image resized to 256
+	transforms.RandomCrop(224),                      # get 224x224 crop from random location
+	transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
+	transforms.ToTensor(),                           # convert the PIL Image to a tensor
+	transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
+						 (0.229, 0.224, 0.225))])
 
 #dataset location in memory
-myPassport_dir = '/media/orlandomelchor/My Passport/datasets/coco-dataset'
-
+data_dir = options['data_dir']
 
 #load data and captions in batches
-dataset = CocoDataset(root='{}/train2017/'.format(myPassport_dir), 
-					  annFile='{}/annotations/captions_train2017.json'.format(myPassport_dir),
-					  filename='{}/coco_annotations.h5'.format(myPassport_dir),
-					  transform=transform,
-					  target_transform=target_transform
-					  )
+dataset = CocoDataset(root='{}/train2017/'.format(data_dir), 
+					  annFile='{}/annotations/captions_train2017.json'.format(data_dir),
+					  filename='{}/coco_annotations.h5'.format(data_dir),
+					  transform=transform)
 
 #number of batches in one load
 batch_size = options['batch_size']
 
 #batch data loader
-dataloader = DataLoader(dataset=dataset, 
+train_loader = DataLoader(dataset=dataset, 
 						batch_size=batch_size,
 						collate_fn=collate,
 						shuffle=True,
+						drop_last=True)
+
+#1 batch data loader
+test_loader = DataLoader(dataset=dataset, 
+						batch_size=1,
+						collate_fn=collate,
+						shuffle=False,
 						drop_last=True)
