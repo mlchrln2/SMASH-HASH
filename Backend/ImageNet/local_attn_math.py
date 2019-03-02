db96849c59b668acc4bd9a9651b360971a2220e3
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-02 15:38:32 -0600

    Finished math but I need to somehow get the weights to play nicely with nans

--- Backend/ImageNet/local_attn_math.py
+++ Backend/ImageNet/local_attn_math.py
@@ -2,18 +2,38 @@ import time
 import torch
 import torch.nn as nn
 
-q = torch.arange(60).view(2,10,3).float()
+batches = 2
+queries = 10
+channels = 3
+words = 2
+word_size = 3
+
+q = torch.arange(60).view(batches,queries,channels).float()
+p_t = torch.tensor([[[0.11],[5.32]],[[1.48],[10]]]).float()
+c_t = torch.arange(2*2*3).view(batches,words,word_size).float()
 S = 10
 D = 2
-p_t_rand = torch.randn(2,2,1)*S
-p_t = torch.tensor([[[0.11],[5.32]],[[1.48],[10]]]).float()
-c_t = torch.arange(2*2*3).view(2,2,3).float()
 
-q_new = nn.ConstantPad2d((0,0,1,0),-760)(q)
-W_q_new = torch.stack([q_new.gather(1,(torch.clamp((p_t+idx+1).long().repeat(1,1,q_new.size(2)),min=0,max=q_new.size(1))%(q_new.size(1)))) for idx in range(-D,D+1)],dim=2)
+lin = nn.Linear(q.size(2),c_t.size(2))
+loss = nn.MSELoss()
+optimizer = torch.optim.Adam(params=lin.parameters(), lr=1e-5, weight_decay=1e-5)
+
+q_new = nn.ConstantPad2d((0,0,1,0),float('nan'))(q)
+q_new = torch.stack([q_new.gather(1,(torch.clamp((p_t+idx+1).long().repeat(1,1,q_new.size(2)),min=0,max=q_new.size(1))%(q_new.size(1)))) for idx in range(-D,D+1)],dim=2)
+W_q_new = lin(q_new)
 a_t = torch.bmm(W_q_new.view(-1,W_q_new.size(2),W_q_new.size(3)),c_t.view(-1,c_t.size(2),1)).view(W_q_new.size(0),W_q_new.size(1),W_q_new.size(2))
-
+hold = torch.isnan(a_t)
+a_t = a_t.clone()
+a_t[hold] = -float('inf')
 softmax = nn.Softmax(2)
 prob = softmax(a_t).unsqueeze(3)
-out = (prob*W_q_new).sum(2)
+old = q_new
+temp = torch.isnan(q_new)
+q_new = q_new.clone()
+q_new[temp] = 0
+out = (prob*q_new).sum(2)
+print(out)
 
+error = loss(out,torch.randn_like(out))
+error.backward()
+optimizer.step()
