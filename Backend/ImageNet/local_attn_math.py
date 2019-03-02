4d2a8785e11dbd87ae92f9b0a8f1fb5e3fc2b230
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-02 15:50:08 -0600

    Figured it out I'm a genius. Pytorch hear me ROAR

--- Backend/ImageNet/local_attn_math.py
+++ Backend/ImageNet/local_attn_math.py
@@ -20,20 +20,20 @@ optimizer = torch.optim.Adam(params=lin.parameters(), lr=1e-5, weight_decay=1e-5
 
 q_new = nn.ConstantPad2d((0,0,1,0),float('nan'))(q)
 q_new = torch.stack([q_new.gather(1,(torch.clamp((p_t+idx+1).long().repeat(1,1,q_new.size(2)),min=0,max=q_new.size(1))%(q_new.size(1)))) for idx in range(-D,D+1)],dim=2)
-W_q_new = lin(q_new)
+temp1 = torch.isnan(q_new)
+q_new_1 = q_new.clone()
+q_new_1[temp1] = 0
+W_q_new = lin(q_new_1)
 a_t = torch.bmm(W_q_new.view(-1,W_q_new.size(2),W_q_new.size(3)),c_t.view(-1,c_t.size(2),1)).view(W_q_new.size(0),W_q_new.size(1),W_q_new.size(2))
-hold = torch.isnan(a_t)
+temp2 = torch.isnan(a_t)
 a_t = a_t.clone()
-a_t[hold] = -float('inf')
+a_t[temp2] = -float('inf')
 softmax = nn.Softmax(2)
 prob = softmax(a_t).unsqueeze(3)
-old = q_new
-temp = torch.isnan(q_new)
-q_new = q_new.clone()
-q_new[temp] = 0
-out = (prob*q_new).sum(2)
-print(out)
+out = (prob*q_new_1).sum(2)
 
 error = loss(out,torch.randn_like(out))
 error.backward()
 optimizer.step()
+
+lin.weight
