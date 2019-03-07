6f1a58daf15d1735bf8b41646745f213025dbcb2
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-07 17:14:46 -0600

    removed variable passing between nested methods and removed some unecessary temps

--- Backend/ImageNet/LocalAttention.py
+++ Backend/ImageNet/LocalAttention.py
@@ -49,11 +49,11 @@ class Attention(nn.Module):
 		return W_attn
 
 class LocalAttention(nn.Module):
-	def __init__(self, query_size, context_size):
+	def __init__(self, query_size, context_size, alignment_size):
 		super(LocalAttention,self).__init__()
 		self.q_size = query_size
 		self.c_size = context_size
-		self.p_size = 100
+		self.p_size = alignment_size
 		self.W_a = nn.Linear(in_features=self.q_size,
 							 out_features=self.c_size,
 							 bias=False)
@@ -66,48 +66,87 @@ class LocalAttention(nn.Module):
 		self.softmax = nn.Softmax(2)
 		self.tanh = nn.Tanh()
 		self.sigmoid = nn.Sigmoid()
-		self.D = 50
+		self.D = 3
 		self.loss = nn.MSELoss()
 		self.optimizer = torch.optim.Adam(params=self.parameters(), lr=1, weight_decay=1e-5)
 	def forward(self,q,c_t):
 		if c_t is None:
 			c_t = q.new_zeros(q.size(0), 1, self.c_size)
-		S = q.size(1)
-		p_t = self.predictive_alignment(S, c_t)
-		p_t = p_t.clone()
-		q = nn.ConstantPad2d((0,0,1,0),float('nan'))(q)
-		q = torch.stack([q.gather(1,(torch.clamp((p_t+idx+1).long().repeat(1,1,q.size(2)),
-						 min=0,max=q.size(1))%q.size(1))) for idx in range(-self.D,self.D+1)],dim=2)
-		W_attn,q1 = self.align(q,c_t)
-		exponent = torch.stack([-2*pow((idx + p_t.long().float() - p_t)/self.D,2) 
-								for idx in range(-self.D,self.D+1)], dim=2)
-		W_attn = W_attn*torch.exp(exponent)
-		s_t = (W_attn*q1).sum(2)
-		return s_t
+		p_t = self.predictive_alignment(q.size(2), c_t)
+		q = nn.ConstantPad1d((1,0),float('nan'))(q)
+		q = q.transpose(1,2)
+		s = torch.clamp(torch.stack([p_t.squeeze(2).long()+idx+1 for idx in range(-self.D,self.D+1)],dim=2),min=0,max=q.size(1))%q.size(1)
+		q = torch.stack([batch[idx] for batch,idx in zip(q,s)])
+		nan_loc = torch.isnan(q[...,0])
+		q[nan_loc] = 0
+		W_attn = self.align(q,c_t,nan_loc)*torch.exp(-2*torch.pow((torch.clamp(s.float()-1,min=0)-p_t)/self.D,2))
+		out = (W_attn.unsqueeze(-1)*q).sum(2)
+		return out
 	def predictive_alignment(self, S, c_t):
 		p_t = S * self.sigmoid(self.V_p(self.tanh(self.W_p(c_t))))
 		return p_t
-	def align(self,q,c_t):
-		s_out, q1 = self.score(q,c_t)
-		W_attn = self.softmax(s_out).unsqueeze(3)
-		return W_attn,q1
-	def score(self,q_new,c_t):
-		temp1 = torch.isnan(q_new[:,:,:,0])
-		q_new_1 = q_new.clone()
-		q_new_1[temp1] = 0
-		W_q_new = self.W_a(q_new_1)
-		W_q_new[temp1] = float('nan')
-		a_t = torch.bmm(W_q_new.view(-1,W_q_new.size(2),W_q_new.size(3)),c_t.view(-1,c_t.size(2),1)).view(W_q_new.size(0),W_q_new.size(1),W_q_new.size(2))
-		temp2 = torch.isnan(a_t)
-		a_t = a_t.clone()
-		a_t[temp2] = -float('inf')
-		return a_t, q_new_1
+	def align(self,q,c_t,nan_loc):
+		a_t = self.score(q,c_t)
+		a_t[nan_loc] = -float('inf')
+		W_attn = self.softmax(a_t)
+		return W_attn
+	def score(self,q,c_t):
+		Wa = self.W_a(q)
+		a_t = torch.bmm(Wa.view(-1,Wa.size(2),Wa.size(3)),c_t.view(-1,c_t.size(2),1)).view(Wa.size(0),Wa.size(1),Wa.size(2))
+		return a_t
 
-model = LocalAttention(3, 512)
+model = LocalAttention(5, 4,100)
+
+q = torch.tensor([[[0.2600, 0.5600, 0.2900, 0.6700, 0.8100],
+		         [0.5900, 0.4000, 0.5300, 0.6800, 0.5200],
+		         [0.5500, 0.8800, 0.5500, 0.1600, 0.6200],
+		         [0.5700, 0.6300, 0.2000, 0.6500, 0.2200],
+		         [0.2000, 0.4200, 0.1100, 0.5900, 0.4600],
+		         [0.3900, 0.5300, 0.5800, 0.2900, 0.9600],
+		         [0.0600, 0.9000, 0.5800, 0.6100, 0.2200],
+		         [0.4800, 0.3000, 0.1200, 0.8600, 0.1600],
+		         [0.1400, 0.6600, 0.9900, 0.8200, 0.8000]],
+
+		        [[0.6500, 0.9400, 0.2500, 0.5000, 0.2500],
+		         [0.2200, 0.6000, 0.6200, 0.3400, 0.7700],
+		         [0.0900, 0.5600, 0.2900, 0.4300, 0.9400],
+		         [0.8000, 0.9500, 0.8100, 0.5400, 0.4600],
+		         [0.5800, 0.7700, 0.3500, 0.0800, 0.7600],
+		         [0.4600, 0.7600, 0.2800, 0.7800, 0.4100],
+		         [0.4100, 0.5000, 0.2800, 0.6200, 0.7300],
+		         [0.4000, 0.9500, 0.8900, 0.7700, 0.8100],
+		         [0.7400, 0.5300, 0.6700, 0.2500, 0.2600]]])
+
+c_t = torch.tensor([[[0.7400, 0.3400, 0.8000, 0.6600],
+			         [0.9200, 0.0500, 0.1700, 0.7700],
+			         [0.0900, 0.5000, 0.8300, 0.9700]],
+
+			        [[0.5000, 0.0400, 0.0900, 0.2300],
+			         [0.2400, 0.2500, 0.9200, 0.5900],
+			         [0.3700, 0.3900, 0.5600, 0.1200]]])
+
+p_t = torch.tensor([[[6.6600],
+			         [1.1700],
+			         [4.9500]],
+
+			        [[7.5600],
+			         [4.9500],
+			         [3.2400]]])
+
+
+model.W_a.weight.data = torch.tensor([[ 0.0823, -0.1433,  0.3603,  0.3888, -0.0696],
+							        [ 0.1195,  0.4177,  0.0176,  0.4141, -0.4175],
+							        [ 0.3725, -0.0063, -0.0640,  0.2831, -0.0994],
+							        [-0.1453, -0.1733, -0.0267, 0.2782, 0.3131]])
+
+
 for i,(img,labels,lengths) in enumerate(dataloader):
 	model.optimizer.zero_grad()
-	output = model(torch.randn(8,224*224,3), torch.randn(8,20,512))
+	q = q.transpose(1,2)
+	output = model(q, c_t)
 	error = model.loss(output, torch.randn_like(output))
+	print(model.W_a.weight)
 	error.backward()
 	model.optimizer.step()
 	print(model.W_a.weight)
+	break
