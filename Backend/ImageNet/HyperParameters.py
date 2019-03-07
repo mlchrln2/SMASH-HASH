0051e04618b06a0e9fefda547dfe0da4e55c4ba7
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-07 17:16:04 -0600

    small changes in the network parameters

--- Backend/ImageNet/HyperParameters.py
+++ Backend/ImageNet/HyperParameters.py
@@ -1,10 +1,10 @@
 options = {
 	'learning_rate' : 1e-3,
-	'batch_size'    : 8, 
+	'batch_size'    : 32, 
 	'num_epochs'    : 100,
 	'channel_size'  : 256,
 	'vocab_size'    : 29549,
-	'embed_size'    : 256,
+	'embed_size'    : 512,
 	'hidden_size'   : 256,
 	'bidirectional' : True,
 	'num_layers'    : 1,
