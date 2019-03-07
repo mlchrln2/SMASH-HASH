d59be112128784054b08d972290f63a5c126f366
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-07 17:42:02 -0600

    I think this might the cleanest it will get

--- Backend/ImageNet/LocalAttention2d.py
+++ Backend/ImageNet/LocalAttention2d.py
@@ -76,11 +76,11 @@ W_a = torch.tensor([[0.0300]])
 
 
 class LocalAttention2d(nn.Module):
-	def __init__(self, query_size, context_size, alignment_size):
+	def __init__(self, query_size, context_size, align_size, window=(1,1)):
 		super(LocalAttention2d,self).__init__()
 		self.q_size = query_size
 		self.c_size = context_size
-		self.p_size = alignment_size
+		self.p_size = align_size
 		self.W_a = nn.Linear(in_features=self.q_size,
 							 out_features=self.c_size,
 							 bias=False)
@@ -93,23 +93,22 @@ class LocalAttention2d(nn.Module):
 		self.softmax = nn.Softmax(2)
 		self.tanh = nn.Tanh()
 		self.sigmoid = nn.Sigmoid()
-		self.R = 1
-		self.C = 2
+		self.R = window[0]
+		self.C = window[1]
 		self.loss = nn.MSELoss()
 		self.optimizer = torch.optim.Adam(params=self.parameters(), lr=1, weight_decay=1e-5)
-	def forward(self,q,c_t,p_t):
+	def forward(self,q,c_t):
 		if c_t is None:
 			c_t = q.new_zeros(q.size(0), 1, self.c_size)
-		#p_t = self.predictive_alignment(q.size(2), c_t)
-		q = q.permute(0,3,1,2)
+		p_t = self.predictive_alignment(q.size(2), c_t)
 		q = nn.ConstantPad2d((1,0,1,0),float('nan'))(q)
 		rows, cols = q.size(2),q.size(3)
-		r = torch.clamp(torch.stack([p_t[...,0].long()+row+1 for row in range(-self.R,self.R+1)],dim=2),min=0,max=rows)%rows
-		c = torch.clamp(torch.stack([p_t[...,1].long()+col+1 for col in range(-self.C,self.C+1)],dim=2),min=0,max=cols)%cols
-		indexes = torch.stack([r[...,i]*cols+c[...,j] for i in range(r.size(-1)) for j in range(c.size(-1))],dim=2)
 		q = q.view(q.size(0),q.size(1),-1)
 		q = q.transpose(1,2)
-		q = torch.stack([batch[idx] for batch,idx in zip(q,indexes)])
+		r = torch.clamp(torch.stack([p_t[...,0].long()+row+1 for row in range(-self.R,self.R+1)],dim=2),min=0,max=rows)%rows
+		c = torch.clamp(torch.stack([p_t[...,1].long()+col+1 for col in range(-self.C,self.C+1)],dim=2),min=0,max=cols)%cols
+		s = torch.stack([r[...,i]*cols+c[...,j] for i in range(r.size(-1)) for j in range(c.size(-1))],dim=2)
+		q = torch.stack([batch[idx] for batch,idx in zip(q,s)])
 		nan_loc = torch.isnan(q[...,0])
 		q[nan_loc] = 0
 		rexp = torch.exp(-2*torch.pow((torch.clamp(r.float()-1,min=0)-p_t[...,0].unsqueeze(-1))/self.R,2))
@@ -124,7 +123,6 @@ class LocalAttention2d(nn.Module):
 	def align(self,q,c_t,nan_loc):
 		a_t = self.score(q,c_t)
 		a_t[nan_loc] = -float('inf')
-		print(a_t)
 		W_attn = self.softmax(a_t)
 		return W_attn
 	def score(self,q,c_t):
@@ -132,12 +130,21 @@ class LocalAttention2d(nn.Module):
 		a_t = torch.bmm(Wa.view(-1,Wa.size(2),Wa.size(3)),c_t.view(-1,c_t.size(2),1)).view(Wa.size(0),Wa.size(1),Wa.size(2))
 		return a_t
 
-model = LocalAttention2d(1,1,100)
+model = LocalAttention2d(query_size=3,
+												 context_size=512,
+												 align_size=10,
+												 window=(20,50))
+embedding = nn.Embedding(num_embeddings=29549,
+												 embedding_dim=512)
 
+
+q = q.permute(0,3,1,2)
+print(model.W_a.weight)
 for i,(img,labels,lengths) in enumerate(dataloader):
+	words = embedding(labels)
 	model.optimizer.zero_grad()
-	output = model(q, c_t, p_t)
+	output = model(img, words)
 	error = model.loss(output, torch.randn_like(output))
 	error.backward()
 	model.optimizer.step()
-	break
+	print(model.W_a.weight)
