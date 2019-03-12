8afb7ca7b21d961241478929492d7d8afbcc3f1a
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-11 19:15:20 -0500

    now have similar visualization as the show, attend, and tell paper

--- Backend/ImageNet/ImageTest.py
+++ Backend/ImageNet/ImageTest.py
@@ -12,22 +12,29 @@ from DataLoader import idx2word
 #modules used for testing and viewing
 import matplotlib.pyplot as plt
 from torchvision.transforms import ToPILImage
+import skimage.transform
 
 #set up a method for drawing the images
 to_img=ToPILImage()
 
 
-def plot(img,frames,caption,i):
+def plot(img,alphas,caption,i):
+	alphas = alphas.unsqueeze(1)
+	alphas = alphas.squeeze(0)
+	img = img.squeeze(0)
+	rgbs = img.repeat(1,1,alphas.size(2)//img.size(2))
+	frames = torch.cat((rgbs,alphas),dim=0)
 	fig = plt.figure(i,figsize=(2,1))
-	img = to_img(img.squeeze(0))
+	img = to_img(img)
 	plt.title(caption)
 	ax1 = fig.add_subplot(2,1,1)
 	plt.imshow(img)
 	ax2 = fig.add_subplot(2,1,2)
-	img2 = to_img(frames)
+	img2 = to_img(rgbs)
 	ax2.set_ylabel(frames.size(1))
 	ax2.set_xlabel(frames.size(2))
 	plt.imshow(img2)
+	plt.imshow(alphas.detach().squeeze().numpy(),cmap='gray',alpha=.75)
 	plt.show()
 	writer.add_figure('plot_{}'.format(i), fig, i, True)
 	plt.close()
@@ -48,7 +55,7 @@ writer = SummaryWriter('ImageNetSummary/')
 error = 0
 gc.collect()
 for i,(img,labels,lengths) in enumerate(dataloader):
-	words, summaries, frames = model.infer(img)
+	words, summaries, alphas = model.infer(img)
 	sentence = ' '.join([idx2word[str(word.item())].value.decode("utf-8") for word in words])
-	plot(img, frames, sentence,i)
+	plot(img, alphas, sentence,i)
 	print('iteration {} of {}'.format(i+1, len(dataloader)), end='\r')
