3eb25fa7220ce37b599e8b1254059924bce93319
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-11 19:13:49 -0500

    put back the normalization preprocessing because now using encoder

--- Backend/ImageNet/DataLoader.py
+++ Backend/ImageNet/DataLoader.py
@@ -83,7 +83,9 @@ transform = transforms.Compose([
 	transforms.Resize(256),                          # smaller edge of image resized to 256
 	transforms.RandomCrop(224),                      # get 224x224 crop from random location
 	transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
-	transforms.ToTensor()                           # convert the PIL Image to a tensor
+	transforms.ToTensor(),                           # convert the PIL Image to a tensor
+	transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
+						 (0.229, 0.224, 0.225))
 ])
 
 #dataset location in memory
