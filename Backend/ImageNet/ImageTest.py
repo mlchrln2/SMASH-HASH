8c44ebddfac6f1d33f3ccc008539b9943be54e45
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-14 14:27:09 -0500

    no more custom directory name for summary writer

--- Backend/ImageNet/ImageTest.py
+++ Backend/ImageNet/ImageTest.py
@@ -49,7 +49,7 @@ model = torch.load('img_embedding_model.pth')
 print('Note model parameters:\n{}'.format(model.parameters))
 
 #create a logger
-writer = SummaryWriter('ImageNetSummary/')
+writer = SummaryWriter()
 #writer.add_graph(model, (torch.randn(1,3,224,224), torch.randint(20,(1,20),dtype=torch.long)),vervose=True)
 
 error = 0
