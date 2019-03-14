d4adaca849e1643930b99c991eabd442fb42d249
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-14 12:39:01 -0500

    the contexts are now the outputs of the rnn and the captions are catted to the attended output before feeding into the rnn

--- Backend/ImageNet/NeuralNetworks.py
+++ Backend/ImageNet/NeuralNetworks.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 from Attention import LocalAttention2d
-from torch.nn.utils.rnn import pack_padded_sequence
+from pack_padded_sequence import pack_padded_sequence
 from torch.nn.utils.rnn import pad_packed_sequence
 
 #user defined modules
@@ -28,10 +28,10 @@ class Image2Caption(nn.Module):
 									  embedding_dim=self.embed_size)
 		self.bi_factor = (2 if self.bidirectional else 1)
 		self.attention = LocalAttention2d(query_size=self.channel_size,
-								   		  context_size=self.embed_size,
+								   		  context_size=self.bi_factor*self.hidden_size,
 								   		  align_size=self.align_size,
 								   		  window=self.window)
-		self.rnn = nn.GRU(input_size=self.channel_size,
+		self.rnn = nn.GRU(input_size=self.channel_size+self.embed_size,
 						  hidden_size=self.hidden_size,
 						  bidirectional=self.bidirectional,
 						  num_layers=self.num_layers,
@@ -39,42 +39,48 @@ class Image2Caption(nn.Module):
 		self.decoder = nn.Linear(in_features=self.hidden_size*self.bi_factor, 
 								 out_features=self.vocab_size)
 		self.optimizer = torch.optim.Adam(params=self.parameters(), 
-										  lr=self.LR,
-										  weight_decay=1e-5)
+										  lr=self.LR)
 		self.criterion = nn.CrossEntropyLoss()
 		self.softmax = nn.Softmax(1)
 	def forward(self, images, captions, lengths):
 		features = self.image_encoder(images)
 		captions = self.embedding(captions)
-		z_inputs = self.attention(features,captions)
-		z_inputs = pack_padded_sequence(z_inputs,lengths,batch_first=True)
-		z_inputs,_ = self.rnn(z_inputs)
-		z_inputs,_ = pad_packed_sequence(z_inputs,batch_first=True)
-		captions = self.decoder(z_inputs)
-		captions = self.softmax(captions.transpose(1,2))
-		return captions
+		captions = captions.transpose(0,1)
+		outputs = torch.zeros(features.size(0),self.vocab_size,captions.size(0))
+		hn,cn = None,None
+		for i,cap in enumerate(captions):
+			feats = self.attention(features,cn)
+			z_inputs = torch.cat((feats,cap.unsqueeze(1)),2)
+			hn,_ = self.rnn(z_inputs,hn)
+			outputs[...,i] = self.decoder(hn.squeeze(1))
+			cn = hn
+			hn = hn.transpose(0,1).view(self.bi_factor,-1,self.hidden_size)
+		return outputs
 	def infer(self, image):
 		features = self.image_encoder(image)
 		idxs = torch.zeros(1,dtype=torch.long)
-		hn = None
 		words = torch.zeros(self.max_len,dtype=torch.long)
 		alphas = torch.zeros(features.size(0),self.max_len,features.size(2),features.size(3))
+		hn,cn = None,None
 		num_words = 0
 		for num_words in range(self.max_len):
-			caption = self.embedding(idxs.unsqueeze(0))
-			z_inputs,alpha = self.attention.infer(features,caption)
-			alphas[:,num_words] = alpha
-			z_inputs,hn = self.rnn(z_inputs,hn)
-			caption = self.decoder(z_inputs.squeeze(1))
-			idxs = torch.argmax(caption,1)
-			words[num_words] = idxs
 			if idxs.item() == 1:
 				break
+			cap = self.embedding(idxs.unsqueeze(0))
+			feats,alpha = self.attention.infer(features,cn)
+			z_inputs = torch.cat((feats,cap),2)
+			hn,_ = self.rnn(z_inputs,hn)
+			output = self.decoder(hn.squeeze(1))
+			cn = hn
+			hn = hn.transpose(0,1).view(self.bi_factor,-1,self.hidden_size)
+			idxs = torch.argmax(output,1)
+			words[num_words] = idxs
+			alphas[:,num_words] = alpha
 		alphas = alphas[:,0].unsqueeze(1) if num_words == 0 else alphas[:,:num_words]
 		alphas = F.interpolate(alphas, size=(image.size(2),image.size(3)), mode='nearest')
 		alphas = alphas.permute(0,2,1,3).contiguous()
 		alphas = alphas.view(alphas.size(0), alphas.size(1),alphas.size(2)*alphas.size(3))
-		words = words[0].unsqueeze(0) if num_words == 0 else words[:num_words]
+		words = words[:num_words]
 		hn = hn.transpose(0,1).contiguous()
 		summaries = hn.view(self.hidden_size*self.bi_factor)
 		return words, summaries, alphas
