a27177a9bb60e572b2258e258a3b393f022f2bbc
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-03-17 17:53:59 -0500

    used a different one word example

--- Backend/ImageNet/TestAttention.py
+++ Backend/ImageNet/TestAttention.py
@@ -9,95 +9,52 @@ from HyperParameters import options
 from DataLoader import train_loader as dataloader
 from Attention import LocalAttention2d
 
-q = torch.tensor([[[[0.9700],
-					[0.1700],
-					[0.6700],
-					[0.0600],
-					[0.9300]],
-
-				 [[0.7100],
-					[0.6000],
-					[0.4300],
-					[0.1600],
-					[0.2300]],
-
-				 [[0.9700],
-					[0.6900],
-					[0.0800],
-					[0.2100],
-					[0.7000]],
-
-				 [[0.4100],
-					[0.2900],
-					[0.7400],
-					[0.3800],
-					[0.8500]]],
+q = torch.tensor([[[[0.6200, 0.1500, 0.5400, 0.5100, 0.7800],
+		          [0.6500, 0.7500, 0.5600, 0.9300, 0.7800],
+		          [0.9400, 0.6300, 0.1100, 0.9600, 0.9700],
+		          [0.0000, 0.3600, 0.8700, 0.3100, 0.7900]]],
 
 
-				[[[0.7200],
-					[0.0900],
-					[0.5100],
-					[0.7600],
-					[0.4800]],
-
-				 [[0.1900],
-					[0.2700],
-					[0.6300],
-					[0.9600],
-					[0.2800]],
-
-				 [[0.3700],
-					[0.2900],
-					[0.2700],
-					[0.3800],
-					[0.9100]],
-
-				 [[0.6400],
-					[0.4000],
-					[0.8200],
-					[0.7100],
-					[0.7300]]]])
-
-p_t = torch.tensor([[[2.2091, 1.5382],
-					 [3.2546, 0.6475],
-					 [3.1470, 4.6770]],
-
-					[[3.3531, 2.4096],
-					 [1.4583, 1.8833],
-					 [0.1498, 0.6317]]])
-
-c_t = torch.tensor([[[0.0100],
-					 [0.5500],
-					 [0.8100]],
-
-					[[0.8900],
-					 [0.8800],
-					 [0.4100]]])
+		        [[[0.0200, 0.7500, 0.3000, 0.7400, 0.3100],
+		          [0.0600, 0.3500, 0.0300, 0.2400, 0.5200],
+		          [0.1700, 0.4500, 0.3200, 0.6000, 0.6500],
+		          [0.7100, 0.7000, 0.3000, 0.4800, 0.3300]]]])
 
 
-W_a = torch.tensor([[0.0300]])
+p_t = torch.tensor([[0.1682, 3.6395],
+			        [3.3280, 1.1567]])
+
+c_t = torch.tensor([[0.7000, 0.6100, 0.9500, 0.0100, 0.0700, 0.4700],
+			        [0.2100, 0.8300, 0.6900, 0.7700, 0.2000, 0.5300]])
+
+
+
+W_a = torch.tensor([[0.3000],
+        [0.0700],
+        [0.7700],
+        [0.3300],
+        [0.1000],
+        [0.3800]])
+
 
 #set up a method for drawing the images
 to_img=ToPILImage()
 
-model = LocalAttention2d(query_size=3,
-						 context_size=512,
-						 align_size=10,
-						 window=(25,25))
-embedding = nn.Embedding(num_embeddings=29549,
-						 embedding_dim=512)
+model = LocalAttention2d(query_size=1,
+						 context_size=6,
+						 window=(3,3))
 
+optimizer = torch.optim.Adam(params=model.parameters(),lr=1e-4)
+loss = nn.BCELoss()
 
-q = q.permute(0,3,1,2)
-#print(model.W_a.weight)
+model.W_a.weight.data = W_a
+print(model.W_a.weight)
 for i,(img,labels,lengths) in enumerate(dataloader):
-	words = embedding(labels)
-	model.optimizer.zero_grad()
-	output = model(img, words)
-	error = model.loss(output, torch.randn_like(output))
-	out, (W_attn, loc) = model.infer(img,words)
-	model.weight_viz(img[0],W_attn[0],loc[0],labels[0])
+	optimizer.zero_grad()
+	output = model(q[0].unsqueeze(0), c_t[0].unsqueeze(0))
+	print(output)
+	error = loss(output, torch.randn_like(output))
 	error.backward()
-	model.optimizer.step()
-	#print(model.W_a.weight)
+	optimizer.step()
+	print(model.W_a.weight)
 	break
