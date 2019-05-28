51635b42ec633a1c8798297d683b02792bcf87b0
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-05-28 07:54:03 -0500

    Improved model loading, saving, and checkpointing. Can currently support CUDA with some CPU programming on my machine.

--- Backend/ImageNet/ImageTrain.py
+++ Backend/ImageNet/ImageTrain.py
@@ -3,6 +3,7 @@
 # dependencies
 import gc
 import sys
+import os
 import torch
 from tensorboardX import SummaryWriter
 
@@ -14,6 +15,7 @@ from DataLoader import VAL_LOADER as VAL_DATA
 from pack_padded_sequence import pack_padded_sequence
 
 # load in hyper-parameters from python file
+MODEL_DIR = OPTIONS['model_dir']
 NUM_EPOCHS = OPTIONS['num_epochs']
 ITR_SAVE = OPTIONS['itr_save']
 DEVICE = OPTIONS['device']
@@ -21,13 +23,22 @@ DEVICE = OPTIONS['device']
 # initialize model and loss function
 MODEL = None
 
-''' to continue training a previously saved model type: "python ImageTrain.py continue"
-to start training a new model type: "python ImageTrain.py restart"'''
+#create model directory if it does not currently exist
+if not os.path.isdir(MODEL_DIR):
+    os.makedirs(MODEL_DIR)
 
-if sys.argv[1] == 'continue':
-    MODEL = torch.load('img_embedding_model.pth')
-elif sys.argv[1] == 'restart':
+''' to continue training a previously saved model type the model directory as a parameter'''
+
+try:
+    CHECKPOINT = torch.load(sys.argv[1])
+    print('Training saved model...')
+    MODEL = CHECKPOINT['model']
+    START_EPOCH = CHECKPOINT['epoch']
+except:
     MODEL = Image2Caption()
+    print('Training new model...')
+    START_EPOCH = -1
+
 print('Note model parameters:\n{}'.format(MODEL.parameters))
 
 # set the mode to train
@@ -42,12 +53,10 @@ WRITER.add_graph(MODEL,
 
 '''train model and update the weights using the pack padded outputs with the pack padded labels.
 Save the model every ITR_SAVE iterations so that the program can be killed if needed.'''
-for epoch in range(NUM_EPOCHS):
+for epoch in range(START_EPOCH + 1, NUM_EPOCHS):
     for i, (img, labels, lengths) in enumerate(TRAIN_DATA):
-        MODEL.optimizer.zero_grad()
-        if DEVICE == 'cuda':
-            torch.cuda.empty_cache()
         gc.collect()
+        MODEL.optimizer.zero_grad()
         predictions = MODEL(img, labels[:, :-1], lengths - 1)
         predictions = pack_padded_sequence(
             predictions, lengths - 1, batch_first=True)[0]
@@ -56,11 +65,15 @@ for epoch in range(NUM_EPOCHS):
         loss = MODEL.criterion(predictions, labels)
         loss.backward()
         MODEL.optimizer.step()
-        del loss, predictions, labels, img, lengths
+        del loss, labels, predictions, img, lengths
+        if DEVICE == 'cuda':
+            torch.cuda.empty_cache()
         print('epoch {} of {} --- iteration {} of {}'.format(epoch + 1, NUM_EPOCHS,
                                                              i + 1, len(TRAIN_DATA)),
               end='\r')
-    torch.save(MODEL, 'img_embedding_model_{}.pth'.format(epoch))
+    torch.save({'epoch': epoch,
+                'model': MODEL
+                }, '{}/img_embedding_model_{}.pth'.format(MODEL_DIR, epoch))
     '''
     error = 0
     for i, (img, labels, lengths) in enumerate(VAL_DATA):
